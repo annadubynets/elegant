a3de613b94eb1ce16b75d3e708078f3d6ab31995
Anna Dubynets <arogulina@gmail.com>
2020-04-29 15:31:34 +0300

    added scroll down

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,10 @@ $("#navToggle").click(function() {
     // this line ▼ prevents content scroll-behind
     $("body").toggleClass("locked");
 });
+
+$(function() {
+    $('.go-to-bottom').click (function() {
+        $('html, body').animate({scrollTop: $('footer').offset().top }, 'slow');
+        return false;
+    });
+});
